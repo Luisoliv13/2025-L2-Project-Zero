4703cc253394e795fc81956a11f26c687e8ee122
Luisoliv13 <luis.deoliveirasilva@horowhenua.school.nz>
2025-02-20 11:47:38 +1300

    Update C_01_Make_Statement.py

--- C_01_Make_Statement.py
+++ C_01_Make_Statement.py
@@ -7,4 +7,4 @@ def make_statement(statement, decoration):
 
 
 #Main Routine goes here
-make_statement(statementS="Programming is Fun!", decoration= "👍")
+make_statement(statement="Programming is Fun!", decoration= "👍")
